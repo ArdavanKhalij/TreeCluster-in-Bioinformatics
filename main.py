769d5de13567752910a3b4f0772534a72929923d
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-05-12 19:19:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,47 +9,40 @@ Leaves = []
 ClusterDiversity = []
 global NodesForDetach
 NodesForDetach = []
-global NodesForDetach2
-NodesForDetach2 = []
 
 
 FILE = open("99_otus_unannotated.tree", "r")
 TreeNewickString = str(FILE.read())
 Tree = Tree(TreeNewickString)
+Tree.delete()
 print(len(Tree.get_leaves()))
 
 
 def μ_tree_distance(Tree):
-    # global NodesForDetach2
-    # NodesForDetach2 = []
-    # for i in NodesForDetach:
-    #     NodesForDetach2.append(i)
-    #     NodesForDetach2 = NodesForDetach2 + i.get_leaves()
-    # NodesForDetach2 = list(dict.fromkeys(NodesForDetach2))
-    Sum = 0
-    # TREE = Tree
-    # for i in NodesForDetach:
-    #     TREE.detach()
-    # print(NodesForDetach2)
     Nodes = Tree.get_leaves()
-    # print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-    # print(Nodes)
-    print("--------------------------")
-    # print(len(NodesForDetach2))
     k = len(Nodes)
-    # print("Before", k)
-    # for i in NodesForDetach2:
-    #     if i in Nodes:
-    #         print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-    #         Nodes.remove(i)
+    if k <= 1:
+        return 0
+    Sum = 0
+    NodesForDetach2 = []
+    for i in NodesForDetach:
+        NodesForDetach2.append(i)
+        NodesForDetach2 = NodesForDetach2 + i.get_leaves()
+    NodesForDetach2 = list(dict.fromkeys(NodesForDetach2))
+    print("--------------------------")
+    print(len(NodesForDetach2))
+    print("Before", k)
+    for i in NodesForDetach2:
+        if i in Nodes:
+            # print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+            Nodes.remove(i)
     N = len(Nodes)
-    # print(N - k)
+    if N <= 1:
+        return 0
+    print(k - N)
     print("after", N)
     for i in range(0, N):
         for j in range(i + 1, N):
-            # if i in NodesForDetach or j in NodesForDetach:
-            #     pass
-            # else:
             Sum = Sum + Nodes[i].get_distance(Nodes[j])/len(Nodes)
     return Sum
 
@@ -66,9 +59,12 @@ def Max_diameter_min_cut_partitioning(Tree):
         if (BR + WR + BL + WL) > THRESHOLD:
             if (BL + WL) <= (BR + WR):
                 # for i in NodesForDetach:
-                #     Tree.detach()
-                NodesForDetach.append(Tree.children[0])
+                    # print(i)
+                    # D = Tree.search_nodes(name = i.name)[0]
+                    # D.detach()
+                # NodesForDetach.append(Tree.children[0])
                 Tree.children[0].detach()
+                NodesForDetach.append(Tree.children[0])
                 ClusterDiversity.append(μ_tree_distance(Tree))
                 BU = BL + WL
                 global PARTS
@@ -76,9 +72,15 @@ def Max_diameter_min_cut_partitioning(Tree):
                 print(PARTS)
             else:
                 # for i in NodesForDetach:
-                #     Tree.delete(i)
-                NodesForDetach.append(Tree.children[1])
+                    # print(i)
+                    # D = Tree.search_nodes(name=i.name)[0]
+                    # D.detach()
+                # NodesForDetach.append(Tree.children[1])
                 Tree.children[1].detach()
+                try:
+                    NodesForDetach.append(Tree.children[1])
+                except:
+                    pass
                 ClusterDiversity.append(μ_tree_distance(Tree))
                 BU = BR + WR
                 PARTS = PARTS + 1
@@ -98,3 +100,38 @@ print(ClusterDiversity)
 print("------------------------------------------")
 print(sum(ClusterDiversity))
 print(sum(ClusterDiversity)/len(ClusterDiversity))
+
+# Results
+# For Deepika to run
+# 0.005 :
+# 0.010 :
+# 0.015 :
+# 0.020 :
+# 0.025 :
+# 0.030 :
+# 0.035 :
+# 0.040 :
+# 0.045 :
+# 0.050 :
+# 0.055 :
+# 0.060 :
+# 0.065 :
+# 0.070 :
+# 0.075 :
+
+# For Ardavan to run
+# 0.080 :
+# 0.085 :
+# 0.090 :
+# 0.095 :
+# 0.100 :
+# 0.105 :
+# 0.110 :
+# 0.115 :
+# 0.120 :
+# 0.125 :
+# 0.130 :
+# 0.135 :
+# 0.140 :
+# 0.145 :
+# 0.150 :
